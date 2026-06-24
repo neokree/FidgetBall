8986ec9e54d1938e649e065b6b0181402a806c8b
NeoKree <neokree@gmail.com>
2026-06-24 18:49:06 +0200

    Initial Commit

---
+++ FidgetBall/FidgetBallApp.swift
@@ -0,0 +1,17 @@
+//
+//  FidgetBallApp.swift
+//  FidgetBall
+//
+//  Created by Fabio Biola on 24/06/26.
+//
+
+import SwiftUI
+
+@main
+struct FidgetBallApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FidgetBallTests/FidgetBallTests.swift
@@ -0,0 +1,17 @@
+//
+//  FidgetBallTests.swift
+//  FidgetBallTests
+//
+//  Created by Fabio Biola on 24/06/26.
+//
+
+import Testing
+@testable import FidgetBall
+
+struct FidgetBallTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
